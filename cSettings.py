ce93b2a1562311cf9e34895613b3bc4d87ccdb29
kxtells <jordi.kstells@gmail.com>
2012-02-18 12:42:01 +0100

    Cleaned the cSettings code

--- cSettings.py
+++ cSettings.py
@@ -1,11 +1,3 @@
-from ConfigParser import SafeConfigParser
-import pygame
-from cAnimSprite import cAnimSprite
-import functions as BF
-import cItemBouncer
-import cItemRecoverLives
-import cMonsterBasher
-import cMonsterFlie
 import shelve
 
 class cSettings:
@@ -29,7 +21,6 @@ class cSettings:
 			Loads the settings file and
 			fills all the settings
 		"""
-		print "load_settings"
 		try:
 			s = open(self.settings_file, 'r');s.close() #file exists check, if not, an exception raises
 			db = shelve.open(self.settings_file)
@@ -56,73 +47,7 @@ class cSettings:
 			the settings file is not found, then saves the
 			new settings file
 		"""
-		print "default_settings"
 		self.fullscreen = False
 		self.username = "DummyName"
 		self.save_settings_file()
 	
-
-	############################
-	#
-	# Interfacing with record files
-	#
-	############################
-	
-	def save_record(self,username,newtime):
-		"""
-			Saves the new record into the proper shelve
-			loads the records into the class attributes self.records and self.player_record_index
-			returns a tuple with the results
-		"""
-		#data pack
-		newdata = (newtime,username)
-		
-		db = shelve.open("db/"+self.uuid)
-		
-		dbrecords = []
-		#recover data if exists
-		if db.has_key("records"):
-			dbrecords = db["records"]
-	
-		#check if a data drop is needed
-		if len(dbrecords) < cLevel._MAX_SAVED_RECORDS:
-			dbrecords.append(newdata)
-		else:
-			dbrecords.sort()
-			worsttime = dbrecords[-1][0]
-			if newtime < worsttime:
-				dbrecords.pop()
-				dbrecords.append(newdata)
-		
-		dbrecords.sort()
-	
-		db["records"] = dbrecords
-	
-		user_index = -1
-		for i,val in enumerate(dbrecords):
-			if dbrecords[i][0] == newtime and dbrecords[i][1] == username:
-				user_index = i
-	
-		db.close()
-
-		self.records = dbrecords
-		self.player_record_index = user_index
-		print user_index
-		return dbrecords,user_index
-
-	def load_records(self):
-		"""
-			Return a tuple (record,name) for a level
-		"""
-		db = shelve.open("db/"+self.uuid)
-		
-		dbrecords = []
-		#recover data if exists
-		if db.has_key("records"):
-			dbrecords = db["records"]
-	
-		self.records = dbrecords
-
-		return dbrecords
-
-
