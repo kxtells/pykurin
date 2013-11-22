e5655e4bacb09213ce812fc5394173c111bc60eb
kxtells <jordi.kstells@gmail.com>
2013-11-22 16:53:51 +0100

    Selection and deleting of items

--- LevelBuilder/tklb/tkintertest.py
+++ LevelBuilder/tklb/tkintertest.py
@@ -88,12 +88,18 @@ class AppUI(Frame):
 
         #Bind Mouseover on Canvas
         self.canvas.bind("<Motion>", self.mouse_motion)
+
+        #Bind Center click to pan
         self.canvas.bind("<B2-Motion>", self.pan_motion)
         self.canvas.bind("<Button-2>", self.pan_start)
 
+        #Bind clicking to select
         self.canvas.bind("<Button-1>", self.canvas_left_click)
         self.canvas.bind("<B1-Motion>", self.canvas_left_click_motion)
         self.canvas.bind("<ButtonRelease-1>", self.canvas_left_click_release)
+
+        #Bind clicking to select
+        self.canvas.bind("<Delete>", self.delete_item)
 
         #PAN
         self.panx  = 0
@@ -131,29 +137,107 @@ class AppUI(Frame):
 
         self.pan_start(event)
 
-
     #
     # Item selection
     #
     def canvas_left_click(self, event):
+        #Once clicked, give keyboard focus to canvas (in case it wasn't set)
+        self.canvas.focus_set()
+
         x = event.x
         y = event.y
+
+        #Delete all selections
+        for item in self.canvas.find_withtag("selection"):
+            self.canvas.delete(item)
+
+        #Create a new selection
+        self.select_item(x,y)
+
+    def select_item(self, x, y):
+        """Selects an item, setting the sitem attribute"""
         c = self.canvas
         items = self.canvas.find_overlapping(x-2,y-2,x+2,y+2)
 
-        #Just interested in the movable items
+        #Just interested in the selectable items
         pitems = []
+
         for i in items:
             if "select" in c.gettags(i):
                 pitems.append(i)
 
-        self.sitem = pitems[0]
+        self.sitem = None
+        if pitems:
+            self.sitem = pitems[0]
 
+        print self.sitem, "Is now selected"
+
+        if self.sitem:
+            c.create_rectangle(c.bbox(self.sitem), tags=("pan","selection"),
+                                                   outline="red", width=2)
+
+
+    #
+    # Item moving
+    #
     def canvas_left_click_motion(self, event):
-        print event
+        print "lc motion"
 
     def canvas_left_click_release(self, event):
-        print event
+        print "lc release"
+
+    #
+    # Remove items
+    #
+    def delete_item(self, event):
+        if not self.sitem:
+            print "No item selected"
+            return
+
+        itype = self.dataids[self.sitem][0]
+        if itype == self.DC.BASHER:
+            dcid = self.dataids[self.sitem][1]
+
+            # Canvas ids of the other basher parts
+            rectid = self.dataids[self.sitem][2]
+            lineid = self.dataids[self.sitem][3]
+
+            #delete from canvas
+            self.canvas.delete(self.sitem)
+            self.canvas.delete(rectid)
+            self.canvas.delete(lineid)
+
+            self.DC.remove_item(itype, dcid)
+
+        else:
+            dcid = self.dataids[self.sitem][1]
+            #Delete from the container
+            self.DC.remove_item(itype, dcid)
+
+            self.canvas.delete(self.sitem)
+
+        self.__update_dataids_after_remove(itype, dcid)
+
+
+    #
+    # Handling references to data ids
+    #
+    def __update_dataids_after_remove(self, itype, dcid):
+        """Update all the indexes after the removal of one
+        of the objects.
+            This is kind of ugly because the ids of datacontainer
+            are just the indexes. So removing one object from there
+            implies invalidating most of the indexes here.
+        """
+        for tkid,dcdata in self.dataids.items():
+            dtype = dcdata[0]
+            if dtype == itype:
+                if dcdata[1] > dcid:
+                    if itype == self.DC.BASHER:
+                        self.dataids[tkid] = (itype, dcdata[1] - 1,
+                                              dcdata[2], dcdata[3])
+                    else:
+                        self.dataids[tkid] = (itype, dcdata[1] - 1)
 
     #
     # LEVEL LOADING AND SAVING
@@ -189,59 +273,72 @@ class AppUI(Frame):
             Create a canvas from the DC.
         """
         canvas = self.canvas
+        dc     = self.DC
         canvas.delete(ALL)
         self.panx = self.pany = 0
 
         canvas.create_image((0,0),
-                                image=self.DC.get_bgimage(), anchor=NW)
+                                image=dc.get_bgimage(), anchor=NW)
         canvas.create_image((0,0),
-                            image=self.DC.get_image(), anchor=NW,
+                            image=dc.get_image(), anchor=NW,
                             tags = ("pan"))
 
         #Filled with pygame RECTS
-        for r in self.DC.bouncers:
-            canvas.create_image((r.x, r.y),
-                                image=self.DC.get_bouncer_image(), anchor=NW,
+        for idx,r in enumerate(dc.bouncers):
+            id = canvas.create_image((r.x, r.y),
+                                image=dc.get_bouncer_image(), anchor=NW,
                                 tags=("select", "move", "bouncer", "delete", "pan"))
 
-        for r in self.DC.lives:
-            canvas.create_image((r.x, r.y),
-                                image=self.DC.get_live_image(), anchor=NW,
+            self.dataids[id] = (dc.BOUNCER, idx)
+
+        for idx,r in enumerate(dc.lives):
+            id = canvas.create_image((r.x, r.y),
+                                image=dc.get_live_image(), anchor=NW,
                                 tags=("select", "move", "delete", "lives", "pan"))
 
-        for r in self.DC.goals:
-            canvas.create_image((r.x, r.y),
-                                image=self.DC.get_goal_image(), anchor=NW,
+            self.dataids[id] = (dc.LIVES, idx)
+
+        for idx,r in enumerate(dc.goals):
+            id = canvas.create_image((r.x, r.y),
+                                image=dc.get_goal_image(), anchor=NW,
                                 tags=("select", "move", "goal", "pan"))
 
-        for r in self.DC.sticks:
-            canvas.create_image((r.x, r.y),
-                                image=self.DC.get_stick_image(), anchor=NW,
+            self.dataids[id] = (dc.GOALS, idx)
+
+        for idx,r in enumerate(dc.sticks):
+            id = canvas.create_image((r.x, r.y),
+                                image=dc.get_stick_image(), anchor=NW,
                                 tags=("select", "move", "stick", "pan")
                                 )
 
-        for i,r in enumerate(self.DC.bashers):
+            self.dataids[id] = (dc.GOALS, idx)
+
+        for idx,r in enumerate(dc.bashers):
             r1 = r
-            r2 = self.DC.bashers_end[i]
+            r2 = dc.bashers_end[idx]
 
             arrow_startx = r1.x + r1.w/2
             arrow_starty = r1.y + r1.h/2
             arrow_endx   = r2.x
             arrow_endy   = r2.y
-            canvas.create_line(arrow_startx,
+            idl = canvas.create_line(arrow_startx,
                                arrow_starty,
                                arrow_endx,
                                arrow_endy,
                                tags = ("pan")
                                )
 
-            canvas.create_rectangle(arrow_endx-5, arrow_endy-5,
+            ids = canvas.create_rectangle(arrow_endx-5, arrow_endy-5,
                                     arrow_endx+5, arrow_endy+5,
                                     tags=("select", "move", "delete", "basher",
-                                          "pan"))
-            canvas.create_image((r1.x, r1.y),
-                                image=self.DC.get_basher_image(), anchor=NW,
+                                          "pan"), fill="black")
+            idb = canvas.create_image((r1.x, r1.y),
+                                image=dc.get_basher_image(), anchor=NW,
                                 tags=("select", "move", "pan"))
+
+            self.dataids[idb] = (dc.BASHER, idx, ids, idl)
+            self.dataids[ids] = (dc.BASHER_END, idx)
+            self.dataids[idl] = (-1, idx)
 
         # Draw the 0,0 cross
             canvas.create_line(10, 0, -10, 0, fill="red", tags=("pan"))
