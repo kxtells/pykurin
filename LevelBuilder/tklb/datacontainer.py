c0706e0406d09f69ce679d1610e674ae70b97238
kxtells <jordi.kstells@gmail.com>
2013-11-25 14:54:38 +0100

    Datacontainer won't add any constant when loading or saving the levels

--- LevelBuilder/tklb/datacontainer.py
+++ LevelBuilder/tklb/datacontainer.py
@@ -57,8 +57,6 @@ class datacontainer:
         #LEGACY
         self.items_pack = [self.bashers,self.bouncers,self.lives,self.goals,self.sticks,self.bashers_end]
 
-
-
     def __load_images(self):
         """Loads the basic set of icons"""
         images = {}
@@ -75,7 +73,7 @@ class datacontainer:
         images["goal"] = ImageTk.PhotoImage(timage)
 
         timage = Image.open("icons/lives.png")
-        images["lives"] = ImageTk.PhotoImage(timage)
+        images["live"] = ImageTk.PhotoImage(timage)
 
         timage = Image.open("icons/stick.png")
         images["stick"] = ImageTk.PhotoImage(timage)
@@ -167,84 +165,32 @@ class datacontainer:
         if itype == self.BASHER:
             del self.items[self.BASHER_END][index]
 
+    def move_item(self, itype, index, nx, ny):
+        self.items[itype][index] = Rect(nx,ny,0,0)
+
     def add_item(self,ident,mx,my):
         #if ident==0:
         #   return True
-        if ident == 0: #basher
-            self.bashers.append(Rect(mx-32,my-32,64,64))
-            self.bashers_end.append(Rect(mx-8 -64,my-8 -64,16,16)) #where basher moves
-        elif ident == 1: #bouncer
-            self.bouncers.append(Rect(mx-16,my-16,32,32))
-        elif ident == 2: #lives
-            self.lives.append(Rect(mx-16,my-16,32,32))
-        elif ident == 3: #Goal
+        if ident == self.BASHER: #basher
+            self.bashers.append(Rect(mx,my,64,64))
+            return len(self.bashers) - 1
+        if ident == self.BASHER_END:
+            self.bashers_end.append(Rect(mx,my,0,0)) #where basher moves
+            return len(self.bashers_end) - 1
+        elif ident == self.BOUNCER: #bouncer
+            self.bouncers.append(Rect(mx,my,32,32))
+            return len(self.bouncers) - 1
+        elif ident == self.LIVES: #lives
+            self.lives.append(Rect(mx,my,32,32))
+            return len(self.lives) - 1
+        elif ident == self.GOALS: #Goal
             if len(self.goals)<1: #only one goal ma friend
-                self.goals.append(Rect(mx-50,my-50,100,100))
-        elif ident == 4: #Start
+                self.goals.append(Rect(mx,my,100,100))
+                return len(self.goals) - 1
+        elif ident == self.STICKS: #Start
             if len(self.sticks)<1: #only one goal ma friend
-                self.sticks.append(Rect(mx-32,my-32,64,64))
-
-    def move_current_item(self,mx,my):
-        if self.selecteditem == None: return False
-        ident = self.selecteditem[0]
-        obj = self.selecteditem[1]
-
-        if ident == 0: #basher
-            self.items_pack[ident][obj] = Rect(mx-32,my-32,64,64)
-        elif ident == 1 or ident == 2: #bouncer and lives
-            self.items_pack[ident][obj] = Rect(mx-16,my-16,32,32)
-        elif ident==3:
-            self.items_pack[ident][obj] = Rect(mx-50,my-50,100,100)
-        elif ident==4:
-            self.items_pack[ident][obj] = Rect(mx-32,my-32,64,64)
-        elif ident==5:
-            self.items_pack[ident][obj] = Rect(mx-8,my-8,16,16)
-
-    def touched_item(self,mx,my):
-        touched = None
-
-        for i,items_arr in enumerate(self.items_pack):
-            for j,item_rect in enumerate(items_arr):
-                if item_rect.contains((mx-2,my-2,4,4)):
-                    touched = i,j
-
-
-        self.selecteditem = touched
-
-        return self.selecteditem!=None
-
-    def touched_selected_item(self,mx,my):
-        if self.selecteditem==None: return
-        ident = self.selecteditem[0]
-        obj = self.selecteditem[1]
-
-        return self.items_pack[ident][obj].contains((mx-2,my-2,4,4))
-
-    def unselect_item(self):
-        self.selecteditem = None
-
-    def get_selected_square(self):
-        if self.selecteditem == None:
-            return None
-        else:
-            st = self.selecteditem[0]
-            si = self.selecteditem[1]
-            return self.items_pack[st][si]
-
-    def delete_selected_item(self):
-        if self.selecteditem != None:
-            st = self.selecteditem[0]
-            si = self.selecteditem[1]
-            self.items_pack[st].pop(si)
-
-            #basher is a double item, so we have to delete that
-            if st == 0: #it's a basher
-                self.items_pack[5].pop(si)
-            elif st == 5: #it's the end of a basher
-                self.items_pack[5].pop(si)
-
-            self.selecteditem = None
-
+                self.sticks.append(Rect(mx,my,64,64))
+                return len(self.sticks) - 1
 
     #
     # Checkers
@@ -360,8 +306,8 @@ class datacontainer:
                 bx,t,by = basher.partition(",")
                 bex,t,bey = basher_end.partition(",")
 
-                self.bashers.append(Rect(int(bx)-32+xp,int(by)-32+yp,64,64))
-                self.bashers_end.append(Rect(int(bex)-8+xp,int(bey)-8+yp,16,16))
+                self.bashers.append(Rect(int(bx)+xp,int(by)+yp,0,0))
+                self.bashers_end.append(Rect(int(bex)+xp,int(bey)+yp,0,0))
         except:
             pass
 
@@ -370,7 +316,7 @@ class datacontainer:
         try:
             for b in parser.items('bouncers'):
                 bx,by = b
-                self.bouncers.append(Rect(int(bx)-16+xp,int(by)-16+yp,32,32))
+                self.bouncers.append(Rect(int(bx)+xp,int(by)+yp,32,32))
         except:
             pass
 
@@ -379,7 +325,7 @@ class datacontainer:
         try:
             for b in parser.items('recovers'):
                 bx,by = b
-                self.lives.append(Rect(int(bx)-16+xp,int(by)-16+yp,32,32))
+                self.lives.append(Rect(int(bx)+xp,int(by)+yp,32,32))
         except:
             pass
 
@@ -387,13 +333,13 @@ class datacontainer:
         del self.sticks[:]
         gx = int(parser.get('options','startx'))
         gy = int(parser.get('options','starty'))
-        self.sticks.append(Rect(int(gx)-32+xp,int(gy)-32+yp,64,64))
+        self.sticks.append(Rect(int(gx)+xp,int(gy)+yp,64,64))
 
     def retrieve_end(self,parser,xp,yp):
         del self.goals[:]
         gx = int(parser.get('options','endx'))
         gy = int(parser.get('options','endy'))
-        self.goals.append(Rect(int(gx)-50+xp,int(gy)-50+yp,100,100))
+        self.goals.append(Rect(int(gx)+xp,int(gy)+yp,100,100))
 
 
     def clear_everything(self):
@@ -428,12 +374,13 @@ class datacontainer:
         f.write("background:"+img+"\n")
         f.write("background2:"+bg+"\n")
 
-        stickx = self.sticks[0][0] +32 -xpadding
-        sticky = self.sticks[0][1] +32 -ypadding
+        stickx = self.sticks[0][0] -xpadding
+        sticky = self.sticks[0][1] -ypadding
+        print stickx, sticky
         f.write("startx:"+str(stickx)+"\n")
         f.write("starty:"+str(sticky)+"\n")
-        goalx = self.goals[0][0] +50 -xpadding
-        goaly = self.goals[0][1] +50 -ypadding
+        goalx = self.goals[0][0] -xpadding
+        goaly = self.goals[0][1] -ypadding
         f.write("endx:"+str(goalx)+"\n")
         f.write("endy:"+str(goaly)+"\n")
         f.write("stick:sticks/stick.png\n") #default value at the moment
@@ -445,14 +392,14 @@ class datacontainer:
 
         f.write("[bouncers]\n")
         for b in self.bouncers:
-            bx = b[0] +16 -xpadding
-            by = b[1] +16 -ypadding
+            bx = b[0] -xpadding
+            by = b[1] -ypadding
             f.write(str(bx)+":"+str(by)+"\n")
 
         f.write("[recovers]\n")
         for b in self.lives:
-            bx = b[0] +16 -xpadding
-            by = b[1] +16 -ypadding
+            bx = b[0] -xpadding
+            by = b[1] -ypadding
             f.write(str(bx)+":"+str(by)+"\n")
 
         f.write("[bashers]\n")
