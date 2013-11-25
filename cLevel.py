6948188f68aa565b17e6f5305202b125fd51af4f
kxtells <jordi.kstells@gmail.com>
2013-11-25 14:54:19 +0100

    Clevel won't add any constant when loading the sprites

--- cLevel.py
+++ cLevel.py
@@ -10,14 +10,14 @@ import shelve
 
 class cLevel:
 	_MAX_SAVED_RECORDS = 5
-	
+
 	def __init__(self,file):
 		parser = SafeConfigParser()
 		parser.read(file)
 
 		self.name 	= parser.get('options','name')
-		self.startx = int(parser.get('options','startx'))-32 #stick is 64 in size
-		self.starty = int(parser.get('options','starty'))-32
+		self.startx = int(parser.get('options','startx'))
+		self.starty = int(parser.get('options','starty'))
 		self.imgcol	= pygame.image.load(parser.get('options','collision')).convert_alpha();
 		self.image 	= pygame.image.load(parser.get('options','background')).convert_alpha();
 		self.bg 	= pygame.image.load(parser.get('options','background2')).convert_alpha();
@@ -31,8 +31,8 @@ class cLevel:
 		gy = int(parser.get('options','endy'))
 		goal_images     =  BF.load_and_slice_sprite(100,100,'goal.png');
 		self.goal_sprite=  cAnimSprite(goal_images,5)
-		self.goal_sprite.move(gx,gy)
-                
+		self.goal_sprite.move(gx + 50,gy + 50) #need the +50 not sure why [TODO]
+
 		#ITEMS LOADING
 		bouncers	=  self.retrieve_bouncer_list(parser)
 		recovers	=  self.retrieve_recover_list(parser)
@@ -80,11 +80,11 @@ class cLevel:
 			tmask = pygame.mask.from_surface(self.imgcol.subsurface(stick.rect))
 
                 	col = stick.mask.overlap(tmask,(0,0))
-                
+
                 	if col == None: return False,0,0
                 	else: return True,col[0]+stick.rect.x,col[1]+stick.rect.y
-		
-		except: 
+
+		except:
 			pass
 
 		return False,i,j
@@ -99,18 +99,18 @@ class cLevel:
 			tmask = pygame.mask.from_surface(self.imgcol.subsurface(stick.rect))
 
                 	col = stick.mask.overlap(tmask,(0,0))
-                
+
                 	if col == None: return False,0,0
                 	else: return True,col[0]+stick.rect.x,col[1]+stick.rect.y
-		
-		except: 
+
+		except:
 			pass
 
-		return False,i,j		
+		return False,i,j
 
 	def stick_in_goal(self,stick):
                 """
-                    Check if the stick center is within the level goal    
+                    Check if the stick center is within the level goal
                 """
 		scx = stick.rect.center[0]
 		scy = stick.rect.center[1]
@@ -128,7 +128,7 @@ class cLevel:
 			for b in parser.items('bouncers'):
 				bx,by = b
 				rot = 0
-				newbouncer = cItemBouncer.cItemBouncer(int(bx)-16,int(by)-16,rot)
+				newbouncer = cItemBouncer.cItemBouncer(int(bx),int(by),rot)
 				bouncer_list.append(newbouncer)
 		except:
 			pass
@@ -145,7 +145,7 @@ class cLevel:
 
 		except:
 			pass
-		
+
 		return recover_list
 
 
@@ -159,11 +159,11 @@ class cLevel:
 				ex = ecoord.partition(',')[0]
 				ey = ecoord.partition(',')[2].partition(';')[0]
 				speed = ecoord.partition(',')[2].partition(';')[2]
-				newbasher = cMonsterBasher.cMonsterBasher(int(sx)-32,int(sy)-32,int(ex)-32,int(ey)-32,int(speed))
+				newbasher = cMonsterBasher.cMonsterBasher(int(sx),int(sy),int(ex),int(ey),int(speed))
 				basher_list.append(newbasher)
 		except:
 			pass
-		
+
 		return basher_list
 
 
@@ -185,7 +185,7 @@ class cLevel:
 	# Interfacing with record files
 	#
 	############################
-	
+
 	def save_record(self,username,newtime):
 		"""
 			Saves the new record into the proper shelve
@@ -194,14 +194,14 @@ class cLevel:
 		"""
 		#data pack
 		newdata = (newtime,username)
-		
+
 		db = shelve.open("db/"+self.uuid)
-		
+
 		dbrecords = []
 		#recover data if exists
 		if db.has_key("records"):
 			dbrecords = db["records"]
-	
+
 		#check if a data drop is needed
 		if len(dbrecords) < cLevel._MAX_SAVED_RECORDS:
 			dbrecords.append(newdata)
@@ -211,16 +211,16 @@ class cLevel:
 			if newtime < worsttime:
 				dbrecords.pop()
 				dbrecords.append(newdata)
-		
+
 		dbrecords.sort()
-	
+
 		db["records"] = dbrecords
-	
+
 		user_index = -1
 		for i,val in enumerate(dbrecords):
 			if dbrecords[i][0] == newtime and dbrecords[i][1] == username:
 				user_index = i
-	
+
 		db.close()
 
 		self.records = dbrecords
@@ -232,12 +232,12 @@ class cLevel:
 			Return a tuple (record,name) for a level
 		"""
 		db = shelve.open("db/"+self.uuid)
-		
+
 		dbrecords = []
 		#recover data if exists
 		if db.has_key("records"):
 			dbrecords = db["records"]
-	
+
 		self.records = dbrecords
 
 		return dbrecords
