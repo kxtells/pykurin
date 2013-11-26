59b37aff91419f42091c0702bbb57e60d2236cbe
kxtells <jordi.kstells@gmail.com>
2013-11-26 12:25:03 +0100

    lbdialogs, containing a finished level dialog. Lets the user choose the name and the backgrounds for the level

--- LevelBuilder/tklb/lbdialogs.py
+++ LevelBuilder/tklb/lbdialogs.py
@@ -6,8 +6,9 @@
 #
 from Tkinter import *
 
-class tkLevelDialog(Toplevel):
+from common_dialogs import *
 
+class tkLevelDialog(Toplevel):
     def __init__(self, parent, title = None, modal=True, datacontainer=None):
         Toplevel.__init__(self, parent)
         self.transient(parent)
@@ -23,28 +24,46 @@ class tkLevelDialog(Toplevel):
 
 
         Label(self, text="Name:").grid(row=0)
-        Label(self, text="Background:").grid(row=1)
-        Label(self, text="Background2:").grid(row=2)
-        Label(self, text="Collisions:").grid(row=3)
+        Label(self, text="Level Image:").grid(row=1)
+        Label(self, text="Static Background:").grid(row=2)
+        Label(self, text="Collisions Image:").grid(row=3)
         Label(self, text="Stick:").grid(row=4)
 
         self.leveltitle = StringVar()
         self.leveltitle.set(self.DC.get_title())
-        self.e0 = Entry(self, textvariable = self.leveltitle)
+        self.e0 = Entry(self, textvariable = self.leveltitle, width=50)
 
 
-        self.background = StringVar()
-        self.background.set(self.DC.get_image_fname())
-        self.e1 = Entry(self, textvariable = self.background)
+        self.image = StringVar()
+        self.image.set(self.DC.get_image_fname())
+        self.e1 = Entry(self, textvariable = self.image, width=50,
+                        state=DISABLED, relief=RIDGE)
 
         self.background2 = StringVar()
         self.background2.set(self.DC.get_background_fname())
-        self.e2 = Entry(self, textvariable = self.background2)
-
+        self.e2 = Entry(self, textvariable = self.background2, width=50,
+                       state=DISABLED, relief=RIDGE)
 
         self.collision = StringVar()
         self.collision.set(self.DC.get_colision_fname())
-        self.e3 = Entry(self, textvariable = self.collision)
+        self.e3 = Entry(self, textvariable = self.collision, width=50,
+                       state=DISABLED, relief=RIDGE)
+
+
+        self.bframe  = Frame(self)
+        self.bok     = Button(self.bframe, text="OK", width=6, command=lambda: self.finish())
+        self.bapply  = Button(self.bframe, text="Apply", width=6, command=lambda: self.apply())
+        self.bcancel = Button(self.bframe, text="Cancel", width=6, command=lambda: self.cancel())
+
+        self.fchooser1 = Button(self, text="F", width=1,
+                                command=lambda: self.fchooser(self.DC.IMAGE))
+        self.fchooser2 = Button(self, text="F", width=1,
+                                command=lambda: self.fchooser(self.DC.BGIMAGE))
+        self.fchooser3 = Button(self, text="F", width=1,
+                                command=lambda: self.fchooser(self.DC.COLIMAGE))
+        #bcancel= Button(toolbar, text="Cancel", width=6, command=self.cancel())
+        #bapply.pack(side=LEFT, padx=2, pady=2)
+        #self.buttons.append(b)
 
         self.e0.grid(row=0, column=1)
         self.e1.grid(row=1, column=1)
@@ -52,5 +71,42 @@ class tkLevelDialog(Toplevel):
         self.e3.grid(row=3, column=1)
         #self.e4.grid(row=4, column=1)
 
+        self.bframe.grid(row=5, column=0, columnspan=3)
+        self.bok.grid(row=0, column=0)
+        self.bapply.grid(row=0, column=1)
+        self.bcancel.grid(row=0, column=2)
+        self.fchooser1.grid(row=1, column=2)
+        self.fchooser2.grid(row=2, column=2)
+        self.fchooser3.grid(row=3, column=2)
+
         if modal:
             self.wait_window(self)
+
+    def apply(self):
+        self.DC.set_image(self.e1.get())
+        self.DC.set_bg_image(self.e2.get())
+        self.DC.set_col_image(self.e3.get())
+        self.DC.set_title(self.e0.get())
+
+    def finish(self):
+        self.apply()
+        self.destroy()
+
+    def cancel(self):
+        self.destroy()
+
+    def fchooser(self, which):
+        """
+            File chooser for the Image, static background and Collision
+        """
+        filename = open_image_chooser("Image chooser",
+                                    self.DC.get_basepath());
+        if not filename:
+            return
+
+        if which == self.DC.IMAGE:
+            self.image.set(filename)
+        elif which == self.DC.BGIMAGE:
+            self.background2.set(filename)
+        elif which == self.DC.COLIMAGE:
+            self.collision.set(filename)
